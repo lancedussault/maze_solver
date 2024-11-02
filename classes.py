f5185a83e2a5361936417f86370ac29782360728
lancedussault <144727107+lancedussault@users.noreply.github.com>
2024-11-02 07:32:51 -0600

    building grid

--- classes.py
+++ classes.py
@@ -1,4 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
+import random
+import time
 
 class Window:
     def __init__(self, width, height):
@@ -87,3 +89,51 @@ class Cell:
 
         line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
         self._window.draw_line(line, fill_color)
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        window,
+    ):
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._window = window
+        
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self._num_cols):
+            column = []
+            for j in range(self._num_rows):
+                cell = Cell(self._window)
+                column.append(cell)
+            self._cells.append(column)
+        
+
+    def _draw_cell(self, i, j):
+        if self.window is None:
+            return 
+        x1 = self.x1 + i * self.cell_size_x
+        y1 = self.y1 + j * self.cell_size_y
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        if self.window is None:
+            return
+        self.window.redraw()
+        time.sleep(0.05)
--- main.py
+++ main.py
@@ -1,31 +1,16 @@
 from classes import *
 
 def main ():
-    window = Window(800, 600)
-    
-    c1 = Cell(window)
-    c1.has_right_wall = False
-    c1.draw(50, 50, 100, 100)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    window = Window(screen_x, screen_y)
 
-    c2 = Cell(window)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(100, 50, 150, 100)
-
-    c1.draw_move(c2)
-
-    c3 = Cell(window)
-    c3.has_top_wall = False
-    c3.has_right_wall = False
-    c3.draw(100, 100, 150, 150)
-
-    c2.draw_move(c3)
-
-    c4 = Cell(window)
-    c4.has_left_wall = False
-    c4.draw(150, 100, 200, 150)
-
-    c3.draw_move(c4, True)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, window)
 
     window.wait_for_close()
 
