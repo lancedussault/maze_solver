4311cd925845bf7e50dcbc00e2bb72387de64c3b
lancedussault <144727107+lancedussault@users.noreply.github.com>
2024-11-01 20:03:13 -0600

    Drew a line through the cells

--- classes.py
+++ classes.py
@@ -40,3 +40,50 @@ class Line:
         self.point_one.x, self.point_one.y, self.point_two.x, self.point_two.y, fill=fill_color, width=2
         )
         
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._window = window
+
+    def draw(self, x1, y1, x2, y2):
+        if self._window is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._window.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._window.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._window.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._window.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self._x1 - self._x2) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length = abs(to_cell._x1 - to_cell._x2) // 2
+        x_center2 = half_length + to_cell._x1
+        y_center2 = half_length + to_cell._y1
+       
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._window.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from classes import Window, Point, Line
+from classes import *
 
 def main ():
-    win = Window(800, 600)
-    line = Line(Point(50, 50), Point(400, 400))
-    line_2 = Line(Point(100, 20), Point(69, 69))
-    win.draw_line(line, "black")
-    win.draw_line(line_2, "red")
-    win.wait_for_close()
+    window = Window(800, 600)
+    
+    c1 = Cell(window)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
 
+    c2 = Cell(window)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(window)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(window)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
+
+    window.wait_for_close()
 
 main()
