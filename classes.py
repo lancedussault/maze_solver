ec9cb692832a6477d862afdef2165335d815bf5e
lancedussault <144727107+lancedussault@users.noreply.github.com>
2024-11-02 14:15:16 -0600

    can now generate a grid

--- classes.py
+++ classes.py
@@ -119,21 +119,24 @@ class Maze:
                 cell = Cell(self._window)
                 column.append(cell)
             self._cells.append(column)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
         
 
     def _draw_cell(self, i, j):
-        if self.window is None:
+        if self._window is None:
             return 
-        x1 = self.x1 + i * self.cell_size_x
-        y1 = self.y1 + j * self.cell_size_y
-        x2 = x1 + self.cell_size_x
-        y2 = y1 + self.cell_size_y
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
 
         self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
 
     def _animate(self):
-        if self.window is None:
+        if self._window is None:
             return
-        self.window.redraw()
+        self._window.redraw()
         time.sleep(0.05)
